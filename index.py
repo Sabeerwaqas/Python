c464ddc7d57d5dd9073c6830797065addd12d51f
Sabeerwaqas <sabeerwaqas050@gmail.com>
2025-03-16 02:33:23 +0500

    Chore: Learn some basics.

---
+++ index.py
@@ -0,0 +1,21 @@
+# -----> Print anything in Pthon <----- #
+
+print("Sabeer Waqas")
+
+# -----> Conditional Statements <----- #
+
+number = 10;
+
+if number >= 10:
+    print("It is >= 10");
+elif number <= 10:
+    print("It is <= 10");
+    
+# -----> Getting user input <----- #
+
+userName = input("Enter youtr name: ");
+print(userName);
+
+x, y = input("Enter 2 values: ").split();
+print(x,y);
+
